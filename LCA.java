36e79cab7dcf3d3875ea83e3adac33ce8011d12c
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-11-21 14:35:53 +0000

    LCA.java

---
+++ LCA.java
@@ -0,0 +1,15 @@
+//LCA
+//for node X, Y, create string {H, a1, b1, c1...X}
+//where X = given node, b = parent node of a, etc, and H = head node
+//when string for X {a1, b1, c1...} and string for Y {a2, b2, c2...} converge
+// ie when {f1...H} = {h2...H}
+//then f1 = h2 = LCA
+
+public class LCA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
