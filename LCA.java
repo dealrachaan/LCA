c6531c375edd5bc8e685800c3d797b8d5e24829f
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-12-15 21:06:56 +0000

    Update LCA.java

--- LCA.java
+++ LCA.java
@@ -5,11 +5,31 @@
 // ie when {f1...H} = {h2...H}
 //then f1 = h2 = LCA
 
-public class LCA {
+public Node LCA(Node root, Node node1, Node node2){
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	if (root==null){
+		return null;
+	}
+	
+	if(root == node1){
+		return node1;
+	}
+	
+	if (root == node2){
+		return node2;
+	}
+	
+	Node left = LCA(root.left, node1, node2);
+	Node right = LCA(root.right, node1, node2);
+	
+	if(left == null && right == null){
+		return null;
+	}else if( left != null && right != null){
+		reutrn root;
+	}else if(left == null){ //process of elimination: right is not null
+		return right;
+	}else{ //when right is null and left is not null
+		return left;
 	}
 
 }
