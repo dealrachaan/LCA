611fc9b7a1877f26437b8be64bb01b067d342123
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-12-16 21:55:47 +0000

    Update LCATest.java

--- LCATest.java
+++ LCATest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 
 public class LCATest {
 	public void testEmptyTree(){ //look for LCA between two non-existent nodes in empty tree
-		BinarySearchTree emptyTree = new BinarySearchTree();
-    LCA(emptyTree, 1, 2);
-	}
+	BinarySearchTree emptyTree = new BinarySearchTree();
+    	LCA(emptyTree, 1, 2);
+}
   
   public void testOneElementTree{ //look for LCA when one element is in tree
     BinarySearchTree oneElementTree = new BinarySearchTree();
@@ -15,9 +15,13 @@ public class LCATest {
     LCA(oneElementTree, 1, 2);
   }
   
-  
-  
-  
+  public void testBothElementsTree{ //look for LCA when both elements present in tree
+	BinarySearchTree bothElementsTree = new BinarySearchTree(); 
+  	bothElementsTree.insert(1);
+	bothElementsTree.insert(2);
+	bothElementsTree.insert(3);
+  	LCA(bothElementsTree, 1, 2);
+  }
 	public void test() {
 		fail("Not yet implemented");
 	}
